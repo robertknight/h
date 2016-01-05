5c9808850bd19cd4dde3d3f3184632b647ac6a8d
Robert Knight <robertknight@gmail.com>
2016-01-05 11:28:31 +0000

    Add additional documentation to promisify() helper

--- h/browser/chrome/lib/util.js
+++ h/browser/chrome/lib/util.js
@@ -1,6 +1,18 @@
 /**
  * Converts an async Chrome API into a function
  * which returns a promise.
+ *
+ * Usage:
+ *   var apiFn = promisify(chrome.someModule.aFunction);
+ *   apiFn(arg1, arg2)
+ *     .then(function (result) { ...handle success  })
+ *     .catch(function (err) { ...handle error })
+ *
+ *
+ * @param fn A Chrome API function whose last argument is a callback
+ *           which is invoked with the result of the query. When this callback
+ *           is invoked, the promise is rejected if chrome.extension.lastError
+ *           is set or resolved with the first argument to the callback otherwise.
  */
 function promisify(fn) {
   return function () {
