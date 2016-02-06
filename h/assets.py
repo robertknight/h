95aa5a953c4954a7b9879d546cde89eb61960e7a
Robert Knight <robertknight@gmail.com>
2016-02-06 15:30:18 +0000

    Remove logic to process JS/CSS in webassets
    webassets is now used exclusively to serve JS/CSS
    assets, not to build them

--- h/assets.py
+++ h/assets.py
@@ -1,62 +1,9 @@
 # -*- coding: utf-8 -*-
 import os
 
-from webassets.filter import ExternalTool, register_filter
 import pyramid
 
 from h._compat import string_types
-
-
-def _locate_npm_bin(name):
-    return './node_modules/.bin/{}'.format(name)
-
-
-# The release versions of webassets upstream don't support extra arguments yet.
-class CleanCSS(ExternalTool):
-    """
-    Minify CSS using `Clean-css <https://github.com/GoalSmashers/clean-css/>`_.
-    """
-
-    name = 'cleancss'
-    options = {
-        'binary': _locate_npm_bin('cleancss'),
-        'extra_args': '--skip-advanced --skip-rebase',
-    }
-
-    def run(self, _in, out, extra_args):
-        args = [_locate_npm_bin('cleancss')] + extra_args
-        self.subprocess(args, out, _in)
-
-    def output(self, _in, out, **kw):
-        self.run(_in, out, [])
-
-    def input(self, _in, out, **kw):
-        extra_args = ['--root', os.path.dirname(kw['source_path'])]
-        self.run(_in, out, extra_args)
-
-register_filter(CleanCSS)
-
-
-class PostCSS(ExternalTool):
-    """ Add vendor prefixes using postcss and autoprefixer.
-
-        webassets does ship with an 'autoprefixer' filter but
-        it does not support the current version of autoprefixer
-        which ships as a postcss plugin rather than standalone
-        tool.
-
-        Using postcss directly from JS will also enable transitioning
-        to a single JS-based tool for all CSS processing.
-    """
-
-    name = 'postcss'
-    max_debug_level = None
-
-    def output(self, _in, out, **kw):
-        args = ['node', 'scripts/postcss-filter.js']
-        self.subprocess(args, out, _in)
-
-register_filter(PostCSS)
 
 
 class AssetRequest(object):
